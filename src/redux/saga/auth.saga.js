2d8f1314a9410067ca9e200420e7ce8939e49e9f
Charmighodasara <charmighodasara9@gmail.com>
2022-08-29 22:00:09 +0530

    changes

--- src/redux/saga/auth.saga.js
+++ src/redux/saga/auth.saga.js
@@ -3,24 +3,24 @@ import { signInApi, signUpApi } from '../../commen/apis/auth.api';
 import * as ActionTypes from '../ActionTypes'
 
 function* signUp(action) {
-   try {
-      const user = yield call(signUpApi, action.payload);
-      console.log(user);
+  try {
+    const user = yield call(signUpApi, action.payload);
+    console.log(user);
     //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
-   } catch (e) {
+  } catch (e) {
     console.log(e);
     //   yield put({type: "USER_FETCH_FAILED", message: e.message});
-   }
+  }
 }
 function* signin(action) {
-   try {
-      const user = yield call(signInApi, action.payload);
-      console.log(user);
+  try {
+    const user = yield call(signInApi, action.payload);
+    console.log(user);
     //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
-   } catch (e) {
+  } catch (e) {
     console.log(e);
     //   yield put({type: "USER_FETCH_FAILED", message: e.message});
-   }
+  }
 }
 
 export function* watchSignup() {
@@ -31,9 +31,9 @@ export function* watchSignin() {
   yield takeEvery(ActionTypes.SIGNIN_USER, signin);
 }
 
-export function* authsaga(){
-    yield all([
-        watchSignup(),
-        watchSignin()
-    ])
+export function* authsaga() {
+  yield all([
+    watchSignup(),
+    watchSignin()
+  ])
 }
