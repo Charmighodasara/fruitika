cc1afbdd2c260ec60fe89a4b67b39f823f732b57
Charmighodasara <charmighodasara9@gmail.com>
2022-09-18 15:08:41 +0530

    changes

--- src/Route/Privateroute.js
+++ src/Route/Privateroute.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 import { isLogin } from '../utility/Index';
 
 function Privateroute({component : Component , ...rest}) {
+    const auth = useSelector(state => state.auth)
     return (
         <Route {...rest} render={props =>(
-            isLogin() ?
+            auth.user !== '' ?
             <Component {...props}/> 
             :
             <Redirect to={'/login-signup'} />
