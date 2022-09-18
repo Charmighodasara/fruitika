5eb6139771c4096a332ffaa12c981f5f7e4e2270
Charmighodasara <charmighodasara9@gmail.com>
2022-09-18 17:54:48 +0530

    changes

--- src/containers/Shop/Shop.js
+++ src/containers/Shop/Shop.js
@@ -65,6 +65,7 @@ function Shop(props) {
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>
                                 </div>
                             </div>
+
                             <div className="col-lg-4 col-md-6 text-center berry">
                                 <div className="single-product-item">
                                     <div className="product-image">
@@ -75,6 +76,7 @@ function Shop(props) {
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>
                                 </div>
                             </div>
+
                             <div className="col-lg-4 col-md-6 text-center lemon">
                                 <div className="single-product-item">
                                     <div className="product-image">
@@ -85,6 +87,7 @@ function Shop(props) {
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>
                                 </div>
                             </div>
+
                             <div className="col-lg-4 col-md-6 text-center">
                                 <div className="single-product-item">
                                     <div className="product-image">
@@ -95,6 +98,7 @@ function Shop(props) {
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>
                                 </div>
                             </div>
+
                             <div className="col-lg-4 col-md-6 text-center">
                                 <div className="single-product-item">
                                     <div className="product-image">
@@ -104,6 +108,7 @@ function Shop(props) {
                                     <p className="product-price"><span>Per Kg</span> 45$ </p>
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
                             </div>
+
                             <div className="col-lg-4 col-md-6 text-center strawberry">
                                 <div className="single-product-item">
                                     <div className="product-image">
@@ -113,8 +118,130 @@ function Shop(props) {
                                     <p className="product-price"><span>Per Kg</span> 80$ </p>
                                     <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
                             </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-7.jpg" alt /></a>
+                                    </div>
+                                    <h3>Apple</h3>
+                                    <p className="product-price"><span>Per Kg</span> 45$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-8.jpg" alt /></a>
+                                    </div>
+                                    <h3>Banana</h3>
+                                    <p className="product-price"><span>Per Kg</span> 40$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-9.jpg" alt /></a>
+                                    </div>
+                                    <h3>Mango</h3>
+                                    <p className="product-price"><span>Per Kg</span> 90$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-10.jpg" alt /></a>
+                                    </div>
+                                    <h3>Orange</h3>
+                                    <p className="product-price"><span>Per Kg</span> 55$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-11.jpg" alt /></a>
+                                    </div>
+                                    <h3>Watermelon</h3>
+                                    <p className="product-price"><span>Per Kg</span> 60$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-12.jpg" alt /></a>
+                                    </div>
+                                    <h3>Peach</h3>
+                                    <p className="product-price"><span>Per Kg</span> 45$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-13.jpg" alt /></a>
+                                    </div>
+                                    <h3>Pomegranate</h3>
+                                    <p className="product-price"><span>Per Kg</span> 35$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-14.jpg" alt /></a>
+                                    </div>
+                                    <h3>Cherry</h3>
+                                    <p className="product-price"><span>Per Kg</span> 45$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-15.jpg" alt /></a>
+                                    </div>
+                                    <h3>Pineapple</h3>
+                                    <p className="product-price"><span>Per Kg</span> 40$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-16.jpg" alt /></a>
+                                    </div>
+                                    <h3>Lychee</h3>
+                                    <p className="product-price"><span>Per Kg</span> 50$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-17.jpg" alt /></a>
+                                    </div>
+                                    <h3>Star Fruit</h3>
+                                    <p className="product-price"><span>Per Kg</span> 35$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 text-center strawberry">
+                                <div className="single-product-item">
+                                    <div className="product-image">
+                                        <a href="single-product.html"><img src="assets/img/products/product-img-18.jpg" alt /></a>
+                                    </div>
+                                    <h3>Papaya</h3>
+                                    <p className="product-price"><span>Per Kg</span> 50$ </p>
+                                    <NavLink to={'/cart'} className="cart-btn"><i className="fas fa-shopping-cart" /> Add to Cart</NavLink>                            </div>
+                            </div>
                         </div>
-                        <div className="row">
+
+                        
+                        {/* <div className="row">
                             <div className="col-lg-12 text-center">
                                 <div className="pagination-wrap">
                                     <ul>
@@ -126,7 +253,7 @@ function Shop(props) {
                                     </ul>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 {/* end products */}
